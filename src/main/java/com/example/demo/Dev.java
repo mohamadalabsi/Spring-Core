ff34dd6733841f6287408de24ec78dee109759b4
mohamadalabsi <absimoda1@gmail.com>
2024-10-04 23:49:32 +0200

    Second step

--- src/main/java/com/example/demo/Dev.java
+++ src/main/java/com/example/demo/Dev.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component   // spring now know , this is the class it ha to manage
+@Component   // spring now know , this is the class it has to manage
 public class Dev {
+//? and remember all variables and attribute should be private (we did that while learning Hibernate , JPA and now Spring ) , the Variables that we used in classes to create tables in the databases eventually
 
+//! 1    it will cause an error
+//       Laptop laptop;//now this is aa reference but not the object (new Laptop()) , and in default it is NULL
+//! 2   it will work but this is not what we want
+//      Laptop laptop = new Laptop();
+
+//! 3   now the right way
+    // field injection
+        @Autowired// this mean to spring that i want to create an  object of dev but dev dependent on laptop so i will connect them  together
+        private   Laptop laptop;
     public  void build(){
+        laptop.compile();
+
         System.out.println("Hello World");
     }
 
